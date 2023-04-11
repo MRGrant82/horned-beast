1b28aeb8f6737bf164aa4421eaddcd08f0a24581
Michael Grant <serus12345@gmail.com>
2023-04-11 13:44:10 -0400

    made changes to hornedBeasts and main.js

--- src/main.js
+++ src/main.js
@@ -2,24 +2,18 @@ import React from 'react';
 import HornedBeast from './hornedBeast';
 
 const hornedBeasts = [
-  {
-    title: 'Spider-Bug',
-    imageUrl: {
-      small: './spider-bug-small.jpg',
-      large: './spider-bug-large.jpg'
+    {
+      title: 'Spider-Bug',
+      imageUrl: './spider-bug.jpg',
+      description: 'A creepy-crawly bug with spider-like legs.',
     },
-    description: 'A creepy-crawly bug with spider-like legs.',
-  },
-  {
-    title: 'Flying-Bug',
-    imageUrl: {
-      small: './flying-bug-small.jpg',
-      large: './flying-bug-large.jpg'
+    {
+      title: 'Flying-Bug',
+      imageUrl: './flying-bug.jpg',
+      description: 'A winged bug that can fly through the air.',
     },
-    description: 'A winged bug that can fly through the air.',
-  },
-
-];
+  ];
+  
 
 class Main extends React.Component {
   render() {
