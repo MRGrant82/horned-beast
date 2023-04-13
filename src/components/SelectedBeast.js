e4818d8942e2139e98ee01102cfba120830e9728
Michael Grant <serus12345@gmail.com>
2023-04-13 14:08:21 -0400

    modal works!  well kind of?

--- src/components/SelectedBeast.js
+++ src/components/SelectedBeast.js
@@ -1,52 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, Button } from 'react-bootstrap';
 
-class SelectedBeast extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: false
-    };
-    this.handleClose = this.handleClose.bind(this);
-    this.handleShow = this.handleShow.bind(this);
-  }
+function SelectedBeast(props) {
+  const [show, setShow] = useState(false);
 
-  handleClose() {
-    this.setState({ show: false });
+  useEffect(() => {
+    if (props.beast) {
+      setShow(true);
+    }
+  }, [props.beast]);
+
+  const handleClose = () => {
+    setShow(false);
     alert('Modal closed');
+  };
+
+  const { beast } = props;
+
+  if (!beast) {
+    return null;
   }
 
-  handleShow() {
-    this.setState({ show: true });
-    alert('Modal opened');
-  }
-
-  render() {
-    const { beast } = this.props;
-
-    return (
-      <div>
-        <Button variant="primary" onClick={this.handleShow}>
-          {beast.title}
-        </Button>
-
-        <Modal show={this.state.show} onHide={this.handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>{beast.title}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <img src={beast.image_url} alt={beast.title} />
-            <p>{beast.description}</p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleClose}>
-              Close
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{beast.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img src={beast.image_url} alt={beast.title} />
+          <p>{beast.description}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 }
 
 export default SelectedBeast;
