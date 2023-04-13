86f91486e3578b78695d38026bdff0cb8922c9c3
Michael Grant <serus12345@gmail.com>
2023-04-13 06:27:10 -0400

    I finally got the key issue to work but I am having trouble getting modal to work

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import imgArr from './data.json';
 import data from './data.json';
-
+import SelectedBeast from './components/SelectedBeast';
 
 function App() {
+  const [selectedBeast, setSelectedBeast] = useState(null);
+
+  function handleBeastSelect(beast) {
+    setSelectedBeast(beast);
+  }
+
   return (
-      <div>
-        <Header />
-        <Main loadContent = {imgArr} />
-        <Footer />
-      </div>
-     );
+    <>
+      <Header />
+      <Main beasts={data} onBeastSelect={handleBeastSelect} />
+      {selectedBeast && <SelectedBeast beast={selectedBeast} onClose={() => setSelectedBeast(null)} />}
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import HornedBeast from './HornedBeast';
 
-
 class Main extends Component {
   render() {
-    const hornedBeasts = this.props.loadContent.map((beast) => (
-      <HornedBeast beastinfo={beast} />
+    const hornedBeasts = this.props.beasts.map((beast) => (
+      <HornedBeast key={beast._id} beastinfo={beast} onBeastSelect={this.props.onBeastSelect} />
     ));
+
     const colBeasts = hornedBeasts.map((beast) => (
-      <Col xs={6} md={4} lg={3} key={beast.key}>
+      <Col xs={6} md={4} lg={3} key={beast.props.beastinfo._id}>
         {beast}
       </Col>
     ));
+    
 
     return (
       <div className="main">
