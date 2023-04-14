79115909de375a8db42605d5fd2b735a99ffa7fb
Michael Grant <serus12345@gmail.com>
2023-04-14 09:51:31 -0400

    added a HornFilter.js and got the modal image to appear better

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import data from './data.json';
 import Header from './components/Header';
 import Main from './components/Main';
 import SelectedBeast from './components/SelectedBeast';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import HornFilter from './components/HornFilter';
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       selectedBeast: null,
+      filteredBeasts: data,
     };
     this.handleBeastSelect = this.handleBeastSelect.bind(this);
     this.handleClose = this.handleClose.bind(this);
+    this.handleFilterChange = this.handleFilterChange.bind(this);
   }
 
   handleBeastSelect(beast) {
@@ -24,12 +27,22 @@ class App extends React.Component {
     this.setState({ selectedBeast: null });
   }
 
+  handleFilterChange(filter) {
+    let filteredBeasts = data;
+    if (filter !== '') {
+      filteredBeasts = data.filter(beast => beast.horns === parseInt(filter));
+    }
+    this.setState({ filteredBeasts: filteredBeasts });
+  }
+
   render() {
+    const { filteredBeasts, selectedBeast } = this.state;
     return (
       <div className="App">
         <Header />
-        <Main beasts={data} onBeastSelect={this.handleBeastSelect} />
-        {this.state.selectedBeast && <SelectedBeast beast={this.state.selectedBeast} onClose={this.handleClose} />}
+        <HornFilter beasts={data} onFilterChange={this.handleFilterChange} />
+        <Main beasts={filteredBeasts} onBeastSelect={this.handleBeastSelect} />
+        {selectedBeast && <SelectedBeast beast={selectedBeast} onClose={this.handleClose} />}
         <Footer />
       </div>
     );
@@ -37,3 +50,46 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+
+
+// import React from 'react';
+// import data from './data.json';
+// import Header from './components/Header';
+// import Main from './components/Main';
+// import SelectedBeast from './components/SelectedBeast';
+// import Footer from './components/Footer';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       selectedBeast: null,
+//     };
+//     this.handleBeastSelect = this.handleBeastSelect.bind(this);
+//     this.handleClose = this.handleClose.bind(this);
+//   }
+
+//   handleBeastSelect(beast) {
+//     this.setState({ selectedBeast: beast });
+//   }
+
+//   handleClose() {
+//     this.setState({ selectedBeast: null });
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <Header />
+//         <Main beasts={data} onBeastSelect={this.handleBeastSelect} />
+//         {this.state.selectedBeast && <SelectedBeast beast={this.state.selectedBeast} onClose={this.handleClose} />}
+//         <Footer />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
